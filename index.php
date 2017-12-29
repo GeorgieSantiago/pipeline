f26355f8f783a05e0235052a686b22b08716c134
GeorgieSantiago <milestoneartisan@gmail.com>
2017-12-29 15:22:20 -0500

    ACL lockdown

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-  require('env.php');
-  require("resource/index.php");
-  new pipeline;
+//Set Headers
+/*
+header('Content-Type: application/json;charset=UTF-8');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: DELETE, HEAD, GET, OPTIONS, POST, PUT');
+header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
+header('Access-Control-Max-Age: 1728000');
+*/
+require('env.php');
+require("resource/index.php");
+new pipeline;
 ?>
--- resource/index.php
+++ resource/index.php
@@ -2,6 +2,7 @@
 require "resource/connect.php";
 require "resource/model.php";
 include "vendor/larapack/dd/src/helper.php";
+
 use Larapacks\Authorization\Traits\RolePermissionsTrait as Auth;
 use illuminate\database as db;
 
@@ -10,6 +11,7 @@ class pipeline extends Connect
     protected $result;
     protected $auth;
     protected $conn;
+
     /*
     Call all resources for pipeline
     call correct action method
@@ -21,26 +23,28 @@ class pipeline extends Connect
         $this->auth();
     }
 
+//Please refactor this shit.
     private function auth()
     {
         if (!isset($_POST['auth'])) {
             die("Not Authorized");
         } else {
-            if ($_SERVER["REQUEST_METHOD"] == 'GET') {
-                die("Not Authorized");
-            } elseif ($_SERVER["REQUEST_METHOD"] == 'POST') {
+            if ($_SERVER["REQUEST_METHOD"] == 'POST') {
                 if ($_POST['auth'] !== 12345) {
                     die("Incorrect Credentials");
                 } else {
                     $this->request($_POST);
                 }
+            } else {
+                die("Something went wrong");
+                exit;
             }
         }
     }
 
     private function request($data)
     {
-        $model  = new Model;
+        $model = new Model;
         $result = $this->conn->querty($model->CreateQuery($data));
 
         if ($result->affected_rows > 0) {
@@ -52,15 +56,16 @@ class pipeline extends Connect
 
     public function result($row)
     {
-        var_dump($row);
         $row = json_encode($row);
-        $pipeline->clean();
+        var_dump($row);
+        $this->clean();
         exit;
     }
 
     public function clean()
     {
-        unset($this);
+        $this->conn->close();
+        unset($this->conn);
     }
 
 }
